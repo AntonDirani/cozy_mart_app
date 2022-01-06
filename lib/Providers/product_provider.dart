793f19f840e1ea7eb19a2e51c0b0c64afd096fb8
anton_dirani <antondirani@outlook.com>
2022-01-06 16:42:24 +0200

    request

--- lib/Providers/product_provider.dart
+++ lib/Providers/product_provider.dart
@@ -1,6 +1,7 @@
 //import 'dart:html';
 import 'dart:io';
 import 'dart:math';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
